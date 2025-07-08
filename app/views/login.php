3d10fe9b4ae6e35e9e0874e0df81a25b8eb665f2
Helama-p <helamarodriguesoujiokita@gmail.com>
2025-07-08 20:37:04 -0300

    login page start

---
+++ app/views/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Faça login</title>
+    <link rel="stylesheet" href="<?php echo $basePath; ?>/assets/login.css">
+   <link rel="shortcut icon" href="<?php echo $basePath; ?>/assets/img/logo.jpeg" type="image/x-icon">
+</head>
+<body>
+    <main>
+        <div class="loginBackgroundf"></div>
+    <form action="./">
+        <div class="title">
+            <h1>🐾Identifique-se para os gatinhos🐾</h1>
+            <h3> Bem-vindo ao Patas e Grãos!</h3>
+        </div>
+        <div Class="inputs">
+            <input type="email" name="email" class="email-box" placeholder="Insira seu email">
+            <input type="password" name="pass" class="pass-box" placeholder="Insira sua senha">
+            <input type="submit" value="Entrar">
+        </div>
+    </form>
+    </main>
+    
+</body>
+</html>
