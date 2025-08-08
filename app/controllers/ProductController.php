e8cd9afca3539397e45a0db5c4bbf876686794c4
Vargas <cauavargas1849@gmail.com>
2025-08-08 15:07:40 -0300

    correcao do backend

--- app/controllers/ProductController.php
+++ app/controllers/ProductController.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 require_once __DIR__ . '/../../config/db.php';
 
-class ProdutoController {
+class ProdutoController
+{
     private PDO $db;
 
     public function __construct()
@@ -9,10 +10,11 @@ class ProdutoController {
         $this->db = (new Database())->getConnection();
     }
 
-    public function getProductById($id) {
+    public function getProductById($id)
+    {
         header('Content-Type: application/json');
         try {
-            if($id <= 0) {
+            if ($id <= 0) {
                 http_response_code(400);
                 return json_encode(['error' => 'ID inválido.']);
             }
@@ -21,7 +23,7 @@ class ProdutoController {
             $produtcs->bindParam(':id', $id, PDO::PARAM_INT);
             $produtcs->execute();
             $produto = $produtcs->fetch(PDO::FETCH_ASSOC);
-            
+
             if (!$produto) {
                 http_response_code(404);
                 return json_encode(['error' => 'Produto não encontrado.']);
@@ -34,7 +36,8 @@ class ProdutoController {
         }
     }
 
-    public function getAllProdutcs() {
+    public function getAllProdutcs()
+    {
         header('Content-Type: application/json');
         try {
             $produtos = $this->db->query('SELECT * FROM produtos')->fetchAll(PDO::FETCH_ASSOC);
@@ -42,21 +45,52 @@ class ProdutoController {
         } catch (PDOException $e) {
             http_response_code(500);
             return json_encode(['error' => 'Erro ao buscar produtos.']);
-        }  
+        }
     }
 
-    public function createProduct(string $name, string $description, float $price, string $img, int $idAdmin) {
+    public function createProduct(string $name, string $description, float $price, string $img, string $category, string $amount, int $idAdmin)
+    {
         header('Content-Type: application/json');
         try {
-            if(empty($name) || empty($description) || $price <= 0 || empty($price) || empty($img) || $idAdmin <= 0 || !is_numeric($price)) {
+            if (empty($name) || empty($description) || $price <= 0 || empty($price) || empty($amount) || empty($category) || empty($img) || $idAdmin <= 0 || !is_numeric($price)) {
                 http_response_code(400);
                 return json_encode(['error' => 'Dados inválidos verifique novamente.']);
             }
-            $query = $this->db->prepare('INSERT INTO produtos (name, desc, price, img, id_admin) VALUES (:name, :description, :price, :img, :idAdmin)');
-            
+            $adminCheck = $this->db->prepare('SELECT * FROM admin WHERE id = :id');
+            $adminCheck->bindParam(':id', $idAdmin, PDO::PARAM_INT);
+            $adminCheck->execute();
+            if (!$adminCheck->fetch(PDO::FETCH_ASSOC)) {
+                http_response_code(404);
+                return json_encode(['error' => 'Administrador não encontrado.']);
+            }
+            $query = $this->db->prepare('INSERT INTO produtos (name, description, price, img, amount, category, id_adm) VALUES (:name, :description, :price, :img, :amount, :category,  :id_adm)');
+            $query->execute([
+                'name' => $name,
+                'description' => $description,
+                'price' => $price,
+                'img' => $img,
+                'amount' => $amount,
+                'category' => $category,
+                'id_adm' => $idAdmin
+            ]);
+            http_response_code(201);
+            return json_encode([
+                'success' => 'Produto criado com sucesso.',
+                'code' => 201,
+                'product' => [
+                    'id' => $this->db->lastInsertId(),
+                    'name' => $name,
+                    'description' => $description,
+                    'price' => $price,
+                    'img' => $img,
+                    'amount' => $amount,
+                    'category' => $category,
+                    'idAdmin' => $idAdmin
+                ]
+            ]);
         } catch (PDOException $e) {
             http_response_code(500);
-            return json_encode(['error' => 'Erro ao criar produto.']);
+            return json_encode(['error' => 'Erro ao criar produto.', 'message' => $e->getMessage()]);
         }
     }
 }
